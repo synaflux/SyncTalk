353d976302ddfaa062973d0f389dfb21ee64f8de
Alex Neamtu <alexneamtu@gmail.com>
2024-05-24 13:44:26 +0300

    Added progress tokens

--- data_utils/hubert.py
+++ data_utils/hubert.py
@@ -3,9 +3,9 @@ import soundfile as sf
 import numpy as np
 import torch
 
-print("Loading the Wav2Vec2 Processor...")
+print("[1/9] Loading the Wav2Vec2 Processor...")
 wav2vec2_processor = Wav2Vec2Processor.from_pretrained("facebook/hubert-large-ls960-ft")
-print("Loading the HuBERT Model...")
+print("[2/9] Loading the HuBERT Model...")
 hubert_model = HubertModel.from_pretrained("facebook/hubert-large-ls960-ft")
 
 
@@ -50,7 +50,7 @@ def get_hubert_from_16k_speech(speech, device="cuda:0"):
     else:
         input_values = input_values_all
     # if input_values.shape[1] != 0:
-    if input_values.shape[1] >= kernel: # if the last batch is shorter than kernel_size, skip it            
+    if input_values.shape[1] >= kernel: # if the last batch is shorter than kernel_size, skip it
         hidden_states = hubert_model(input_values).last_hidden_state # [B=1, T=pts//320, hid=1024]
         res_lst.append(hidden_states[0])
     ret = torch.cat(res_lst, dim=0).cpu() # [T, 1024]
@@ -81,12 +81,17 @@ args = parser.parse_args()
 
 wav_name = args.wav
 
+print('[3/9]')
 speech, sr = sf.read(wav_name)
+print('[4/9]')
 speech_16k = librosa.resample(speech, orig_sr=sr, target_sr=16000)
-print("SR: {} to {}".format(sr, 16000))
+print("[5/9] SR: {} to {}".format(sr, 16000))
 # print(speech.shape, speech_16k.shape)
 
+print('[6/9]')
 hubert_hidden = get_hubert_from_16k_speech(speech_16k)
+print('[7/9]')
 hubert_hidden = make_even_first_dim(hubert_hidden).reshape(-1, 2, 1024)
+print('[8/9]')
 np.save(wav_name.replace('.wav', '_hu.npy'), hubert_hidden.detach().numpy())
-print(hubert_hidden.detach().numpy().shape)
+print(f'[8/9] {hubert_hidden.detach().numpy().shape}')
