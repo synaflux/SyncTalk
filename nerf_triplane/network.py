f0dff785b2a313a513c1408341e1de46d2619070
Alex Cazacu <alexandru.cazacu92@gmail.com>
2024-08-13 15:19:44 +0300

    [DIG-93] Investigate the Synctalk  bug

--- nerf_triplane/network.py
+++ nerf_triplane/network.py
@@ -383,7 +383,12 @@ class NeRFNetwork(NeRFRenderer):
 
         h = self.sigma_net(h)
 
-        sigma = torch.exp(h[..., 0])
+        # sigma = torch.exp(h[..., 0])
+        # Applied fix to address loss=NaN issue caused by numerical instability in the exponential calculation.
+        # The fix clamps the input to a safe range (-9, 9) to prevent overflow in FP16.
+        # TODO: Adjust the -9, 9 clamp range dynamically for FP32 support
+        sigma = torch.exp(torch.clamp(h[..., 0], -9, 9))
+
         geo_feat = h[..., 1:]
 
         return {
